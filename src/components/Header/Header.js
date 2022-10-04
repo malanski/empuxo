0f029af4ac9ec84879a8a85ff1db7b0daa809d7a
Ulisses Malanski <malanskiwork@gamil.com>
2022-10-03 21:49:13 -0300

    add Header audioplayer with HOOKS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import { Nav } from '../Nav/Nav'
+import { PlayerHeader } from '../PlayerHeader/PlayerHeader'
 import './Header.scss'
+import tracks from "../PlayerHeader/tracks";
+
 
 export function Header () {
     return (
         <div className='header'>
             <h1 title='Ulisses Malanski Music 2021'>Empuxo</h1>
+            <PlayerHeader  tracks={tracks} />
             <Nav />
         </div>
     )
---
+++ src/components/PlayerHeader/tracks.js
@@ -0,0 +1,136 @@
+import marmotaAudio from '../../assets/audio/marmota.mp3'
+import cabreiragemAudio from '../../assets/audio/cabreiragem.mp3'
+import ondePossoTeVerAudio from '../../assets/audio/ondePossoTeVer.mp3'
+import camaleaoAudio from '../../assets/audio/camaleao.mp3'
+import viajanteAudio from '../../assets/audio/viajante.mp3'
+import hidraAudio from '../../assets/audio/hidra.mp3'
+import ditadoPopularAudio from '../../assets/audio/ditadoPopular.mp3'
+import soAmetadeAudio from '../../assets/audio/soAmetade.mp3'
+import camaraoAudio from '../../assets/audio/camarao.mp3'
+import casuloAudio from '../../assets/audio/casulo.mp3'
+import qualehAudio from '../../assets/audio/qualeh.mp3'
+import aphantasiaAudio from '../../assets/audio/aphantasia.mp3'
+import anoPassadoAudio from '../../assets/audio/anoPassado.mp3'
+
+import marmota from '../../assets/marmota.jpg'
+import cabreiragem from '../../assets/cabreiragem.jpg'
+import ondePossoTeVer from '../../assets/ondePossoTeVer.jpg'
+import camaleao from '../../assets/camaleao.jpg'
+import viajante from '../../assets/viajante.png'
+import hidra from '../../assets/hidra.jpg'
+import ditadoPopular from '../../assets/ditadoPopular.png'
+import soAmetade from '../../assets/soAmetade.jpg'
+import camarao from '../../assets/camarao.jpg'
+import casulo from '../../assets/casulo.jpg'
+import qualeh from '../../assets/qualeh.png'
+import aphantasia from '../../assets/aphantasia.jpg'
+import anoPassado from '../../assets/anoPassado.jpg'
+
+const trackFiles = [
+    {
+      title: 'Marmota',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: marmotaAudio,
+      length: '3:44',
+      color: '2f4cca',
+      image: marmota
+    },
+    {
+      title: 'Cabreiragem',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: cabreiragemAudio,
+      length: '3:38',
+      color: '#00aeb0',
+      image: cabreiragem
+    },
+    {
+      title: 'Onde Posso Te Ver',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: ondePossoTeVerAudio,
+      length: '3:37',
+      color: '#00aeb0',
+      image: ondePossoTeVer
+    },
+    {
+      title: 'Camaleão',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: camaleaoAudio,
+      length: '3:35',
+      color: '#00aeb0',
+      image: camaleao
+    },
+    {
+      title: 'Viajante',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: viajanteAudio,
+      length: '4:37',
+      color: '#B30E00',
+      image: viajante
+    },
+    {
+      title: 'Hidra',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: hidraAudio,
+      length: '4:13',
+      color: '#ffb77a',
+      image: hidra
+    },
+    {
+      title: 'Ditado Popular',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: ditadoPopularAudio,
+      length: '4:06',
+      color: '##5f9fff',
+      image: ditadoPopular
+    },
+    {
+      title: 'Só a Metade',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: soAmetadeAudio,
+      length: '6:20',
+      color: '#40A918',
+      image: soAmetade
+    },
+    {
+      title: 'Camarão',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: camaraoAudio,
+      length: '4:10',
+      color: '#7F005F',
+      image: camarao
+    },
+    {
+      title: 'Casulo',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: casuloAudio,
+      length: '4:33',
+      color: '#00aeb0',
+      image: casulo
+    },
+    {
+      title: 'Qual é',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: qualehAudio,
+      length: '4:14',
+      color: '#E3006A',
+      image: qualeh
+    },
+    {
+      title: 'Aphantasia',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: aphantasiaAudio,
+      length: '5:05',
+      color: '#00aeb0',
+      image: aphantasia
+    },
+    {
+      title: 'Ano Passado',
+      artist: 'Empuxo - Ulisses Malanski',
+      audioSrc: anoPassadoAudio,
+      length: '3:55',
+      color: '#00aeb0',
+      image: anoPassado
+    },
+  ];
+  
+  export default trackFiles;
